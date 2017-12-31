fa1b6ebe0beb88c37f006e18bbbd207e7495fdde
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-12-31 10:20:56 +0100

    New unit tests for equals and hashcode methods

--- src/test/java/cabanas/garcia/ismael/storeroom/module/product/domain/model/ProductIdShould.java
+++ src/test/java/cabanas/garcia/ismael/storeroom/module/product/domain/model/ProductIdShould.java
@@ -6,14 +6,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public final class ProductIdShould {
 
+    public static final String UUID1 = "uuid1";
+
     @Test public void
-    be_equals() {
-        ProductId productId = ProductId.builder("uuid1").build();
-        ProductId otherProductId = ProductId.builder("uuid1").build();
+    be_reflexive() {
+        ProductId productId = ProductId.builder(UUID1).build();
 
-        boolean actual = productId.equals(otherProductId);
+        assertThat(productId).isEqualTo(productId);
+    }
 
-        assertThat(actual).isTrue();
+    @Test public void
+    be_symetric() {
+        ProductId productId = ProductId.builder(UUID1).build();
+        ProductId otherProductId = ProductId.builder(UUID1).build();
+
+        assertThat(productId).isEqualTo(otherProductId);
+        assertThat(otherProductId).isEqualTo(productId);
+    }
+
+    @Test public void
+    be_transitive() {
+        ProductId oneProductId = ProductId.builder(UUID1).build();
+        ProductId secondProductId = ProductId.builder(UUID1).build();
+        ProductId thirdProductId = ProductId.builder(UUID1).build();
+
+        assertThat(oneProductId).isEqualTo(secondProductId);
+        assertThat(secondProductId).isEqualTo(thirdProductId);
+        assertThat(oneProductId).isEqualTo(thirdProductId);
+    }
+
+    @Test public void
+    not_equal_when_compare_with_null() {
+        ProductId oneProductId = ProductId.builder(UUID1).build();
+        ProductId secondProductId = null;
+
+        assertThat(oneProductId.equals(secondProductId)).isFalse();
     }
 
     @Test public void
@@ -24,6 +51,21 @@ public final class ProductIdShould {
         boolean actual = productId.equals(otherProductId);
 
         assertThat(actual).isFalse();
+    }
+
+    @Test public void
+    hashCode_consistency() {
+        ProductId oneProductId = ProductId.builder(UUID1).build();
+
+        assertThat(oneProductId.hashCode()).isEqualTo(oneProductId.hashCode());
+    }
+
+    @Test public void
+    hashCode_be_equals() {
+        ProductId oneProductId = ProductId.builder(UUID1).build();
+        ProductId secondProductId = ProductId.builder(UUID1).build();
+
+        assertThat(oneProductId.hashCode()).isEqualTo(secondProductId.hashCode());
     }
 
 }
