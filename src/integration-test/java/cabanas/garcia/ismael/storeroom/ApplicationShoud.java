b9e8f3dbba890fc6ba7dd15273b376e9e4ba71c3
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-01-03 12:32:31 +0100

    IT for verifying the context load of Spring Boot application

---
+++ src/integration-test/java/cabanas/garcia/ismael/storeroom/ApplicationShoud.java
@@ -0,0 +1,19 @@
+package cabanas.garcia.ismael.storeroom;
+
+import cabanas.garcia.ismael.storeroom.module.product.Application;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@Category(IntegrationTests.class)
+public class ApplicationShoud {
+
+    @Test public void
+    context_load() {
+
+    }
+}
